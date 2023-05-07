2acb4e2a4e4725011889edf6c2d4e1a202618403
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-05-07 18:01:49 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(arg1=['Antonia', 'Nuru', 'Amari', 'Mo']){
+    return arg1.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(arg1=['Antonia', 'Nuru', 'Amari', 'Mo']){
+    return arg1.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(arg2){
+    return function(fare) {
+        return arg2 * fare
+    }
+}
+
+function fareDoubler(x) {
+    const rando = createFareMultiplier
+    return rando(2)(x)
+}
+
+function fareTripler(x) {
+    const rando = createFareMultiplier
+    return rando(3)(x)
+}
+
+function selectDifferentDrivers(a1=['Antonia', 'Nuru', 'Amari', 'Mo'], a2) {
+    return a2(a1)
+    }
